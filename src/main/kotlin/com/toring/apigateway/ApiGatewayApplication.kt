dba069c4eaa50da186eb772d83e99e3b250aecdb
Hyojeong Yu <40685291+hjyu94@users.noreply.github.com>
2022-01-06 23:26:08 +0900

    Add: eureka client setting (#2)
    Co-authored-by: Hyojeong-Yu <hjeong@lunit.io>

--- src/main/kotlin/com/toring/apigateway/ApiGatewayApplication.kt
+++ src/main/kotlin/com/toring/apigateway/ApiGatewayApplication.kt
@@ -2,7 +2,9 @@ package com.toring.apigateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
+@EnableEurekaClient
 @SpringBootApplication
 class ApiGatewayApplication
 
